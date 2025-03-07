5e16f48415adfe1909db7abf7cbd9754eee9f90b
dumialex32 <dumitrasalexandru26@gmail.com>
2025-03-07 17:46:07 +0100

    feat: add dropdown menu for small screen size

--- src/components/dashboard/LinksDropdown.tsx
+++ src/components/dashboard/LinksDropdown.tsx
@@ -1,5 +1,41 @@
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "../ui/button";
+import { AlignLeft } from "lucide-react";
+import { links } from "@/utils/links";
+import Link from "next/link";
+
 const LinksDropdown = () => {
-  return <div>linksdroopdown</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="lg:hidden">
+        <Button variant="outline" size="icon">
+          <AlignLeft />
+          <span className="sr-only">Toggle links</span>
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent
+        className="w-52 lg:hidden"
+        sideOffset={25}
+        align="start"
+      >
+        {links.map((link) => {
+          return (
+            <DropdownMenuItem asChild key={link.href}>
+              <Link className="w-full" href={link.href}>
+                {link.label}
+              </Link>
+            </DropdownMenuItem>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
 
 export default LinksDropdown;
