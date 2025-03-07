4cf69175fb3c5ac82d6c3a19877b64687ac0a207
dumialex32 <dumitrasalexandru26@gmail.com>
2025-03-07 17:52:42 +0100

    fix: resolve spacing issue in DashboardNavBar where justify-between was not working

--- src/components/dashboard/DashboardNav.tsx
+++ src/components/dashboard/DashboardNav.tsx
@@ -1,13 +1,15 @@
-import ThemeToggle from "../ThemeToggle";
+import ThemeToggle from "../theme/ThemeToggle";
 import LinksDropdown from "./LinksDropdown";
 import { UserButton } from "@clerk/nextjs";
 
 const DashboardNavBar = () => {
   return (
     <nav className="flex justify-between items-center bg-muted py-4 sm:px-16 lg:px-24 px-4">
-      <LinksDropdown />
+      <div>
+        <LinksDropdown />
+      </div>
 
-      <div className="flex gap-4 items-center">
+      <div className="flex gap-4 items-center justify-self-center">
         <ThemeToggle />
         <UserButton />
       </div>
