1e36998d7e93fb0ff32c3b29d5356327213e4e0e
dumialex32 <dumitrasalexandru26@gmail.com>
2025-03-09 22:55:02 +0100

    feat: add createJobAction for job creation

---
+++ src/utils/actions.ts
@@ -0,0 +1,42 @@
+"use server";
+
+import {
+  createJobFormSchema,
+  CreateJobFormValues,
+  JobType,
+} from "@/types/formTypes/createJobFormTypes";
+import prisma from "./db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const authAndRedirect = async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/");
+  }
+
+  return userId;
+};
+
+export const createJobAction = async (
+  values: CreateJobFormValues
+): Promise<JobType | null> => {
+  try {
+    createJobFormSchema.parse(values);
+
+    const userId = await authAndRedirect();
+
+    const job: JobType = await prisma.job.create({
+      data: {
+        ...values,
+        clerkId: userId,
+      },
+    });
+
+    return job;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+};
