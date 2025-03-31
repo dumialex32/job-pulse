c6dc01bef498a922303def860e37c6ed6592cbb9
dumialex32 <dumitrasalexandru26@gmail.com>
2025-03-31 19:08:28 +0200

    refactor(CustomSelect): add form compatibility via hidden input and name prop

--- src/components/CustomSelect.tsx
+++ src/components/CustomSelect.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   Select,
   SelectContent,
@@ -9,37 +10,63 @@ import {
 
 type CustomSelect<T extends string | number> = {
   options: { label: string; value: T }[];
-  value: T;
-  onValueChange: (value: T) => void;
-  placeholder: string;
+  value?: T;
+  onValueChange?: (value: T) => void;
+  placeholder?: string;
+  name?: string;
+  defaultValue?: T;
 };
 
 const CustomSelect = <T extends string | number>({
   value,
   options,
-  placeholder,
+  placeholder = "Select an option",
   onValueChange,
+  name,
+  defaultValue,
 }: CustomSelect<T>) => {
+  // local state for uncontrolled mode (when `value` is not provided)
+  const [selectedValue, setSelectedValue] = useState<T | undefined>(
+    defaultValue
+  );
+
+  console.log(selectedValue);
+
+  const handleChange = (val: string) => {
+    const newValue = val as T;
+
+    if (!value) {
+      setSelectedValue(newValue);
+    }
+
+    onValueChange?.(newValue);
+  };
+
   return (
-    <Select
-      value={String(value)}
-      onValueChange={(val) => onValueChange(val as T)}
-    >
-      <SelectTrigger className="max-w-[200px]">
-        <SelectValue placeholder={placeholder} />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          {options.map((option) => {
-            return (
-              <SelectItem key={option.value} value={String(option.value)}>
-                {option.label}
+    <>
+      {/* hidden input ensures value is included in form submissions */}
+      {name && (
+        <input type="hidden" name={name} value={value ?? selectedValue} />
+      )}
+
+      <Select
+        value={value ? String(value) : String(selectedValue) || undefined}
+        onValueChange={handleChange}
+      >
+        <SelectTrigger className="w-full bg-background">
+          <SelectValue placeholder={placeholder} />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            {options.map(({ label, value }) => (
+              <SelectItem key={value} value={String(value)}>
+                {label}
               </SelectItem>
-            );
-          })}
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </>
   );
 };
 
