2fd7a983a7b200b4728653b9dd4ee94a2db00f78
dumialex32 <dumitrasalexandru26@gmail.com>
2025-04-01 02:18:47 +0200

    refactor(results): optimize rendering logic

--- src/components/pagination/Results.tsx
+++ src/components/pagination/Results.tsx
@@ -7,20 +7,20 @@ const Results = ({
   itemsPerPage,
   resource,
 }: ResultsProps) => {
+  if (count === 0)
+    return <div className="px-4 py-2">No {pluralize(resource)} found.</div>;
+
   const startItem = (currentPage - 1) * itemsPerPage + 1;
   const endItem = Math.min(currentPage * itemsPerPage, count);
-
   const resourceDisplay = pluralize(resource, count);
 
   return (
     <div className="px-4 py-2">
-      <span>
-        Showing{" "}
-        <span className="font-bold">
-          {startItem}-{endItem}
-        </span>{" "}
-        from <span className="font-semibold">{count}</span> {resourceDisplay}
-      </span>
+      <p>
+        Showing <span className="font-bold">{startItem}</span>
+        {startItem !== endItem && ` - ${endItem}`} of{" "}
+        <span className="font-bold">{count}</span> {resourceDisplay}
+      </p>
     </div>
   );
 };
